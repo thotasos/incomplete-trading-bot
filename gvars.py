1ea155fffd313f5fb4f211aa2f1bd15529eb7d59
thotasos <76566989+thotasos@users.noreply.github.com>
2021-03-07 22:19:23 -0800

    Update gvars.py

--- gvars.py
+++ gvars.py
@@ -7,7 +7,7 @@
 from pathlib import Path
 from datetime import datetime
 
-MAX_WORKERS = 10 # max threads at a time
+MAX_WORKERS = 10 # max threads at a time 
 
 gainRatio = 1.5 # takeProfit = -stopLoss*gainRatio
 stopLossMargin = 0.05 # extra margin for the stop loss
